b36c79f9607a9058ad4ddd28c644053e24f63ec6
Morgan McDermott <morganmcdermott@gmail.com>
2013-12-01 17:27:56 -0800

    Codegenz

---
+++ asg5/codegen.cc
@@ -0,0 +1,8 @@
+#include "codegen.h"
+
+//Foo meow meow meow meow meow meow
+void codegen(string filename, astree t, SymbolTable symTable){
+
+
+
+}
---
+++ asg5/codegen.h
@@ -0,0 +1,19 @@
+#indef _CODEGEN_H_
+#define _CODEGEN_H_
+
+#include "astree.h"
+#include "symtable.h"
+#include "typechecker.h"
+
+typedef struct codegenParams {
+  FILE *output;
+  SymbolTable symTable;
+} codegenParams; 
+
+void codegen(string filename, astree t, SymbolTable symTable);
+
+
+
+
+
+#endif
